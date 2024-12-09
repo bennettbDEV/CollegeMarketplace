af7d1415c9d47899effcb53998abec053554bc98
Chase Mayberry <156947914+chasejmReal@users.noreply.github.com>
2024-12-09 14:08:03 -0600

    ADDED: home icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,42 +1,42 @@
+//Navbar.jsx
 import React from 'react';
 import './styles/Navbar.css';
 import userIcon from '../assets/usericon.png';
 import settingsIcon from '../assets/settingsicon.png';
-import { use } from 'react';
+import homeIcon from '../assets/homeicon.png'; // Import the home icon
 
 const Navbar = () => {
   return (
-
-<nav className="navbar">
-  <div className="navbar-left">
-    <a href="/" className="logo">
-      ISU Marketplace
-    </a>
-  </div>
-  <div className="navbar-center">
-    <ul className="nav-links">
-    <li>
-        <a href="/createlisting">Create Listing</a>
-      </li>
-      <li>
-        <a href="/saved">Saved</a>
-      </li>
-      <li>
-        <a href="/messages">Messages</a>
-      </li>
-    </ul>
-  </div>
-  <div className="navbar-right">
-    <a href="/profile" className="profile-icon">
-        
-        <img src= {userIcon}/>
-    </a>
-    <a href="/settings" className="settings">
-    <img src= {settingsIcon}/>
-    </a>
-  </div>
-</nav>
-);
+    <nav className="navbar">
+      <div className="navbar-left">
+        <a href="/" className="logo">
+          <img src={homeIcon} alt="Home" className="home-icon" /> {/* Home Icon */}
+          ISU Marketplace
+        </a>
+      </div>
+      <div className="navbar-center">
+        <ul className="nav-links">
+          <li>
+            <a href="/createlisting">Create Listing</a>
+          </li>
+          <li>
+            <a href="/saved">Saved</a>
+          </li>
+          <li>
+            <a href="/messages">Messages</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <a href="/profile" className="profile-icon">
+          <img src={userIcon} alt="Profile" />
+        </a>
+        <a href="/settings" className="settings">
+          <img src={settingsIcon} alt="Settings" />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
